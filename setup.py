1b3091de01001a688b3f78fc2e8e8a514e3b2f5e
Thijs van Ede <t.s.vanede@utwente.nl>
2020-02-03 11:45:23 -0800

    Updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="appscanner",
-    version="1.0.1",
+    version="1.0.2",
     author="Thijs van Ede",
     author_email="t.s.vanede@utwente.nl",
     description="AppScanner: Automatic Fingerprinting of Smartphone Apps from "
